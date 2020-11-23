190327b50f5685132db76dc7f5d3800307da7b25
SlarVer <mkasakovich@mail.ru>
2020-11-23 13:29:25 +0300

    App initialized

---
+++ src/main/java/by/bsu/kas/PasswordSaver/PasswordSaverApplication.java
@@ -0,0 +1,13 @@
+package by.bsu.kas.PasswordSaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordSaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordSaverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/bsu/kas/PasswordSaver/PasswordSaverApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsu.kas.PasswordSaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordSaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
